d4fea19ae60367db4d38cd205c5d0dca3dc9abd3
Mattias Bergström <mattias.bergstrom@dohi.se>
2014-10-29 21:31:11 +0100

    Added everything except the last step (animation and particle effect)

---
+++ step2/Assets/Scripts/Character.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	public float jumpForce;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			this.rigidbody2D.AddForce(Vector2.up * jumpForce);
+		}
+	}
+
+	void FixedUpdate() {
+		if (this.rigidbody2D.velocity.x < speed) {
+			this.rigidbody2D.AddForce(Vector2.right * 100);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.gameObject.tag == "Deadly") {
+			Application.LoadLevel(0);
+		}
+	}
+}
---
+++ step4/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = this.transform.position;
+		position.x = target.transform.position.x;
+		this.transform.position = position;
+	}
+}
